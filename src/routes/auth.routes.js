5abf5ac606bbe536dd503f5901eb8c504902f3d1
kshitijdev24 <kshitij.chetan030174@gmail.com>
2026-03-02 13:44:48 +0530

    fix: correct route handler for user registration

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const authController=require('../controllers/auth.Controllers')
 const router = express.Router()
-router.post('/register',authController)
+router.post('/register',authController.registerUser)
 module.exports  = router
