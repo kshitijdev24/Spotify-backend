efa6d0dc00db859ffa08714ef444042624cf405d
kshitijdev24 <kshitij.chetan030174@gmail.com>
2026-03-13 15:53:28 +0530

    fix: format code for consistency in auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,26 +1,25 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 async function authArtist(req, res, next) {
-    const token = req.cookies.token;
-    if (!token) {
-        return res.status(401).json({
-            message: "Unauthorize access"
-        })
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorize access",
+    });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (decoded.role !== "artist") {
+      return res.status(403).json({
+        message: "You dont have access",
+      });
     }
-    try {
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
-        if (decoded.role !== 'artist') {
-            return res.status(403).json({
-                message: "You dont have access"
-            })
-        }
-        next();
-    } catch (error) {
-        console.log(error);
-        return res.status(401).json({
-            message: "Unauthorized"
-        })
-    }
-    
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
 }
